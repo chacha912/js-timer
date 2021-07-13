f04dc812e752f130436bbca7df5bb89144652ef8
Sangwon <jackoss@naver.com>
2021-07-13 16:59:51 +0900

    feat: add render laps func add render laps function

--- src/watch.js
+++ src/watch.js
@@ -20,4 +20,41 @@ document.querySelector('.stopwatch').onclick = () => {
       $display.textContent = formatElapsedTime(elapsedTime);
     };
   })();
+
+  const renderLaps = (() => {
+    const $laps = document.querySelector('.stopwatch > .timer-laps');
+
+    const createLapElement = (newLap, index) => {
+      const $fragment = document.createDocumentFragment();
+      const $index = document.createElement('div');
+      $index.textContent = index;
+      $fragment.appendChild($index);
+
+      const $newLap = document.createElement('div');
+      $newLap.textContent = formatElapsedTime(newLap);
+      $fragment.appendChild($newLap);
+
+      $laps.appendChild($fragment);
+      $laps.style.display = 'block';
+    };
+
+    const removeLaps = () => {
+      document
+        .querySelectorAll('.timer-laps > .timer-lap')
+        .forEach($lap => $lap.remove());
+
+      $laps.style.display = 'none';
+    };
+
+    return () => {
+      const { length } = laps;
+
+      if (length) {
+        const newLap = laps[length - 1];
+        createLapElement(newLap, length);
+      } else {
+        removeLaps();
+      }
+    };
+  })();
 };
