287b4196a278de616121d405fd687dfefde020c2
Sangwon <jackoss@naver.com>
2021-07-13 16:23:12 +0900

    feat: add format elapsed time util func add util func of formatting stime display

--- src/watch.js
+++ src/watch.js
@@ -1,11 +1,15 @@
-/* eslint-disable prefer-const */
 /* ==== DOMs ==== */
+document.querySelector('.stopwatch').onclick = () => {
+  const isRunning = false;
+  const lapsedTime = { hh: 0, mm: 0, ss: 0, ms: 0 };
+  const laps = [];
 
-/* === state === */
-let isRunning = false;
-let lapsedTime = { hh: 0, mm: 0, ss: 0, ms: 0 };
-let laps = [];
-
-/* === functions === */
-
-/* === event hadlers === */
+  const formatElapsedTime = (() => {
+    const formatTwoDigits = n => (n < 10 ? '0' + n : n + '');
+    const formatOneDigit = n => parseInt(n / 10, 10);
+    return ({ hh, mm, ss, ms }) =>
+      `${formatTwoDigits(hh)}:${formatTwoDigits(mm)}:${formatTwoDigits(
+        ss
+      )}:${formatOneDigit(ms)}`;
+  })();
+};
