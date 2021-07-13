4ff3dcadcfd1fed391537aea61b986bd1204ce06
Sangwon <jackoss@naver.com>
2021-07-13 16:16:21 +0900

    feat: initialize state set initial states

---
+++ src/watch.js
@@ -0,0 +1,11 @@
+/* eslint-disable prefer-const */
+/* ==== DOMs ==== */
+
+/* === state === */
+let isRunning = false;
+let lapsedTime = { hh: 0, mm: 0, ss: 0, ms: 0 };
+let laps = [];
+
+/* === functions === */
+
+/* === event hadlers === */
